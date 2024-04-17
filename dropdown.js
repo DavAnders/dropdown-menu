ed3739029f9088c4419d95e7ef304cc4c7f5dad5
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-17 17:26:19 -0500

    Implement dropdown menu

---
+++ dropdown.js
@@ -0,0 +1,31 @@
+// dropdown.js for use in case of wanting to be used as a module
+export default function initializeDropdowns() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const dropdowns = document.querySelectorAll(".dropbtn");
+
+    dropdowns.forEach(function (btn) {
+      btn.onclick = function () {
+        const dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+        } else {
+          closeAllDropdowns();
+          dropdownContent.style.display = "block";
+        }
+      };
+    });
+
+    window.onclick = function (event) {
+      if (!event.target.matches(".dropbtn")) {
+        closeAllDropdowns();
+      }
+    };
+
+    function closeAllDropdowns() {
+      const dropdowns = document.querySelectorAll(".dropdown-content");
+      dropdowns.forEach(function (dropdown) {
+        dropdown.style.display = "none";
+      });
+    }
+  });
+}
